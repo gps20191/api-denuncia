c0293823777ab43bf42aed974ff140d19863a0cb
JoséGuilherme <guislima47@gmail.com>
2019-04-25 20:27:51 -0300

    update ComplainController store

--- app/Controllers/Http/ComplaintController.js
+++ app/Controllers/Http/ComplaintController.js
@@ -8,16 +8,19 @@ const Env = use('Env')
 class ComplaintController {
 
   async store({request, response, params}){
-    const data = request.all()
+    const {image} = request.all()
     var headers = { 'Authorization':  Env.get('IMGUR_CLIENT_ID') }
 
-    axios.post(imgurUpload, data.image , {headers: headers})
-      .then(function (response) {
-        console.log(response.status)
-      })
-      .catch(function (error) {
-        console.log(error)
-      });
+    const res = await axios.post(imgurUpload, image , {headers: headers})
+    const {status, success, data: responseData} = await res
+
+    // if (success){
+    //  salvar imagem e dados no banco
+    // }
+
+
+    return { status }
+
   }
 }
 
